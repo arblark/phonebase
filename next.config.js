2d1c300d1c44c67aaca38734d626c41752ca9832
arblark <vladimir.sokol17@gmail.com>
2024-10-25 16:19:09 +0300

    fix env in next config

--- next.config.js
+++ next.config.js
@@ -8,6 +8,10 @@ const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ['bufferutil', 'utf-8-validate'],
   },
+  env: {
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  },
 };
 
 module.exports = nextConfig;
